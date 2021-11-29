f11bed950fb864f8aaf71ea562dc36bd236f6c85
Alexander Williams <alexander.williams@knights.ucf.edu>
2021-11-29 14:21:59 -0500

    added header file

---
+++ src/App.h
@@ -0,0 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Alexander Williams
+ */
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int checkEven(int input);
